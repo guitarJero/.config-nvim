253d7f99a1a87831887982db4404fe6a0d542b8e
Jero Ahumada <jero@iwell.info>
2024-01-18 16:48:14 +0000

    Customized lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,19 +1,42 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  --
-  --   "nvim-lualine/lualine.nvim",
-  --   event = "VeryLazy",
-  --   opts = function()
-  --     return {
-  --       --[[add your custom lualine config here]]
-  --     }
-  --   end,
-  -- }
-  opts = {
-    theme = 'ayu_dark',
-    encoding = function()
-      local ret, _ = (vim.bo.fenc or vim.go.enc):gsub("^utf%-8$", "")
-      return ret
-    end,
-  },
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "meuter/lualine-so-fancy.nvim",
+    },
+    opts = {
+        options = {
+            theme = "iceberg_dark",
+            component_separators = { left = "│", right = "│" },
+            section_separators = { left = "", right = "" },
+            globalstatus = true,
+            refresh = {
+                statusline = 100,
+            },
+        },
+        sections = {
+            lualine_a = {
+                { "fancy_mode", width = 3 }
+            },
+            lualine_b = {
+                { "fancy_branch" },
+                { "fancy_diff" },
+            },
+            lualine_c = {
+                { "fancy_cwd", substitute_home = true }
+            },
+            lualine_x = {
+                { "fancy_macro" },
+                { "fancy_diagnostics" },
+                { "fancy_searchcount" },
+                { "fancy_location" },
+            },
+            lualine_y = {
+                { "fancy_filetype", ts_icon = "" }
+            },
+            lualine_z = {
+                { "fancy_lsp_servers" }
+            },
+        }
+    },
 }

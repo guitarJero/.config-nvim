3f26c1140556a9879d06fd094051f71b01155595
Jero Ahumada <jero@iwell.info>
2024-03-12 14:20:29 -0700

    Added search for hidden files in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -29,6 +29,12 @@ return {
 		local keymap = vim.keymap -- for conciseness
 
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		keymap.set(
+			"n",
+			"<leader>fh",
+			'<cmd>lua require"telescope.builtin".find_files({hidden=true, no_ignore=true})<cr>',
+			{ desc = "Fuzzy find files in cwd" }
+		)
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Grep string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
